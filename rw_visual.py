5f438d066bb3cf53079f3fc3050b29f64bbd10a8
pavel.pek <pavel.pek@bisimulations.com>
2022-01-16 15:57:48 +0100

    rw_visual.py to represent the random walk

---
+++ rw_visual.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    # Plot the point in the walk
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
